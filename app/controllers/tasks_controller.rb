1470385afb6160ad370d8f25b4ac75eb261345be
shinichi.hirata <taro.kirameki@techacademy.jp>
2020-10-01 15:20:40 +0000

    add data

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show 
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new 
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が投稿されました'
+      redirect_to @task
+    else
+      flash[:danger] = 'Task が投稿されません'
+      render :new
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+     @task = Task.find(params[:id])
+     
+    if @task.update(task_params)
+      flash[:success] = 'タスクが編集されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが編集されませんでした'
+      render :new
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:sucess] = "タスクが削除されました"
+    redirect_to task_path
+  end
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:content)
+end
+
+  
+

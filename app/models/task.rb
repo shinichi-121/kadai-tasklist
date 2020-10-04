de28cdcd58f2d1492142ea2f8fa9b1d959276041
shinichi.hirata <taro.kirameki@techacademy.jp>
2020-10-04 15:11:24 +0000

    add column2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 225 } 
-  validates :status, presence: true, length: { maximum: 255 }
+  validates :status, presence:true, length: { maximum: 10 } 
 end
+
+
+
+

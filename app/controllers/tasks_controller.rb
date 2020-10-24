de9955cd2a050f94d59b50ee6751c198396619e7
shinichi.hirata <taro.kirameki@techacademy.jp>
2020-10-24 10:10:29 +0000

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show,:edit,:update,:destroy]
   before_action :require_user_logged_in, only: [:index, :show]
+   before_action :correct_user, only:  [:show,:edit,:destroy]
  
   
   
@@ -55,6 +56,13 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url
+    end
   end
   
 end
